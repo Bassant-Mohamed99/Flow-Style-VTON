ad7a87db4c8c743f18bfb57957939d77eb14e188
Bassant-Mohamed99 <161893193+Bassant-Mohamed99@users.noreply.github.com>
2025-06-09 17:10:02 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -12,6 +12,9 @@ import torch.nn.functional as F
 from torchvision import utils
 from util import flow_util
 
+
+
+
 opt = TestOptions().parse()
 
 
@@ -34,6 +37,30 @@ def de_offset(s_grid):
     offset = torch.cat((offset_y,offset_x),0)
     
     return  offset
+def tta_forward(generator, warped_cloth, edge, real_image):
+    outputs = []
+
+    inp = torch.cat([real_image, warped_cloth, edge], dim=1)
+    outputs.append(generator(inp))
+
+    # Horizontal flip
+    wc_flip = torch.flip(warped_cloth, dims=[3])
+    ri_flip = torch.flip(real_image, dims=[3])
+    edge_flip = torch.flip(edge, dims=[3])
+    inp_flip = torch.cat([ri_flip, wc_flip, edge_flip], dim=1)
+    out_flip = generator(inp_flip)
+    out_flip = torch.flip(out_flip, dims=[3])
+
+    return torch.mean(torch.stack(outputs + [out_flip]), dim=0)
+
+def edge_aware_filter(tensor_img):
+    img = tensor_img.squeeze(0).permute(1, 2, 0).cpu().numpy()
+    img = np.clip(img * 255, 0, 255).astype(np.uint8)
+    filtered = cv2.edgePreservingFilter(img, flags=1, sigma_s=60, sigma_r=0.4)
+    filtered = filtered.astype(np.float32) / 255.0
+    return torch.from_numpy(filtered).permute(2, 0, 1).unsqueeze(0).to(tensor_img.device)
+
+
 
 start_epoch, epoch_iter = 1, 0
 
@@ -84,13 +111,13 @@ for epoch in range(1,2):
         warped_edge = F.grid_sample(edge.cuda(), last_flow.permute(0, 2, 3, 1),
                           mode='bilinear', padding_mode='zeros')
 
-        gen_inputs = torch.cat([real_image.cuda(), warped_cloth, warped_edge], 1)
-        gen_outputs = gen_model(gen_inputs)
+        gen_outputs = tta_forward(gen_model, warped_cloth, warped_edge, real_image.cuda())
         p_rendered, m_composite = torch.split(gen_outputs, [3, 1], 1)
         p_rendered = torch.tanh(p_rendered)
         m_composite = torch.sigmoid(m_composite)
         m_composite = m_composite * warped_edge
         p_tryon = warped_cloth * m_composite + p_rendered * (1 - m_composite)
+        p_tryon = edge_aware_filter(p_tryon)
 
         path = 'results/' + opt.name
         os.makedirs(path, exist_ok=True)
@@ -101,7 +128,8 @@ for epoch in range(1,2):
         if step % 1 == 0:
             
             ## save try-on image only
-
+            # Apply edge-aware smoothing before saving (optional)
+            p_tryon = edge_aware_filter(p_tryon)            
             utils.save_image(
                 p_tryon,
                 os.path.join('./our_t_results', data['p_name'][0]),
